a9b8ba369e2b6e3cfd98c0fbc3cbc3b9b2e8a452
D-Benz <36527173+D-Benz@users.noreply.github.com>
2018-09-06 20:55:54 -0700

    Quick changes, new command

---
+++ cmds/admin.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args)  => {
+    const logChannel = bot.channels.find("name", "admin-log");
+    const ifAdmin = message.author.hasPermission("ADMINISTRATOR");
+
+    if(!ifAdmin) return message.author.send("Hi, I'm Mr. Meeseeks and you don't have `ADMINISTRATOR`");
+
+    const embed = new Discord.RichEmbed()
+        .setTitle("Admin commands! OOOOOOH WEEEEEEEEEEE")
+        .setColor('RANDOM')
+        .setFooter("Beta bot | v0.0.4.20.69", bot.user.displayAvatarURL)
+        .setTimestamp()
+        .addField("!announce", "Make an announcement!")
+        .addField("!ban", "When a user decides to fuck up everyone's life.")
+        .addField("!kick", "When a user is being an edgy 12-year-old fag.")
+        .addField("!newrole", "Create a new role!")
+        .addField("!role", "Assign some fuckboi a role.")
+        .addField("!mute", "That annoying bitch being annoying? Mute them.")
+        .addField("!unmute", "Giving them the silent treatment must have worked.")
+    await message.author.send('', {embed}).catch(error => logChannel.send(`admin command error: ${error}`));
+
+    return 0;
+}
+
+module.exports.help = {
+    name: "admin"
+}
--- cmds/help.js
+++ cmds/help.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = (bot, message, args) => {
+    const logChannel = bot.channels.find("name", "admin-log");
 
     const embed = new Discord.RichEmbed()
     .setTitle("User commands!")
@@ -8,14 +9,12 @@ module.exports.run = (bot, message, args) => {
     .setFooter("Beta bot | v0.0.4.20.69", bot.user.displayAvatarURL)
     .setTimestamp()
     .addField("!add", "For when you want to add your butt buddy.")
-    .addField("!block", "For when some little shit is being annoying. {WIP}")
-    .addField("!unblock","Give that little shit a second chance.{WIP}")
     .addField("!palindrome", "See if a string a letters spells the same backwards!")
     .addField("!serverinfo", "Get info about this basic ass server.")
     .addField("!userinfo", "Info about your stupid ass.")
     .addField("!define", "For when you want to find definitions for your weak memes.")
 
-message.channel.send('', {embed}).catch(error => console.log(`help command error: ${error}`));
+message.channel.send('', {embed}).catch(error => logChannel.send(`help command error: ${error}`));
 
 }
 
